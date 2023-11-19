4307d911e3f8928f2845fdd664c529ae74596217
Tamal Das <tamalcodes@gmail.com>
2023-11-19 21:47:55 +0530

    feat: getting files from old repo

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import {Toaster} from 'react-hot-toast'
+
+store.subscribe(() => {});
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Toaster/>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
